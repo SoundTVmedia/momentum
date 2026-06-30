081c6ed3deddd0b392d8f52d6b31ba7a970474db
SoundTVmedia <wes@soundtvmedia.com>
2026-06-30 16:52:08 -0400

    cap sync

---
+++ ios/App/FeedbackAppTests/FeedbackAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  FeedbackAppTests.swift
+//  FeedbackAppTests
+//
+//  Created by Wes on 6/30/26.
+//
+
+import Testing
+
+struct FeedbackAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
